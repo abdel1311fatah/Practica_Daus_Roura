bf4b4a981e1da4da3c70b4c23a506b98ba291e95
Abdel13Fatah <abdel13fatah@gmail.com>
2023-09-18 13:39:31 +0200

    arreglar el tirar_daus

--- src/main/java/org/example/GenerarHistograma.java
+++ src/main/java/org/example/GenerarHistograma.java
@@ -5,22 +5,34 @@ import java.util.Random;
 public class GenerarHistograma {
     Random r1 = new Random();
     Random r2 = new Random();
-    public int [] [] generar_daus(){
+    public int generar_dau1(){
 
         int d1 = r1.nextInt(6) + 1;
-        int d2 = r2.nextInt(6) + 1;
-        int [][] daus = new int[d1][d2];
 
-        return daus;
+        return d1;
 
     }
-    public int tirar_daus(int[][] daus, int vegades){
 
-        int resultat = daus [0][0] + daus [0][1];
+    public int generar_dau2(){
 
-        System.out.println("Resultat: " + resultat);
+        int d2 = r2.nextInt(6) + 1;
 
-        return resultat;
+        return d2;
+
+    }
+    public String tirar_daus(int vegades){
+
+        String llista = null, asteriscos = null;
+        for (int i = 0; i<= vegades; i++) {
+            int resultat = generar_dau1() + generar_dau2();
+            llista = resultat + ": ";
+            for (int x = 0; x <= resultat; x++){
+                asteriscos += "*";
+            }
+            llista += asteriscos;
+        }
+
+        return llista;
     }
 
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,12 +5,60 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
+        int vegades = obtindreInt("Quantes vegades vols tirar els daus? ");
         GenerarHistograma generarHistograma = new GenerarHistograma();
 
-        System.out.println(generarHistograma.generar_daus());
+        System.out.println(generarHistograma.tirar_daus(vegades));
 
-        generarHistograma.tirar_daus(generarHistograma.generar_daus(), 2);
 
     }
+
+    public String obtindreString(String text) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print(text);
+        String text_usuari = sc.nextLine();
+
+        return text_usuari;
+    }
+
+    public double obtindreDouble(String text) {
+        boolean TipusCorrecte;
+        Scanner sc = new Scanner(System.in);
+        double num_usuari = 0.0;
+
+        do {
+            System.out.print(text);
+            TipusCorrecte = sc.hasNextDouble();
+            if (!TipusCorrecte) {
+                sc.nextLine();
+                System.out.println("Error Valor no valid");
+            } else {
+                num_usuari = sc.nextDouble();
+            }
+        } while (!TipusCorrecte);
+        return num_usuari;
+    }
+
+    public static int obtindreInt(String text) {
+
+        boolean TipusCorrecte;
+        Scanner sc = new Scanner(System.in);
+        int num_usuari = 0;
+
+        do {
+            System.out.print(text);
+            TipusCorrecte = sc.hasNextInt();
+            if (!TipusCorrecte) {
+                sc.nextLine();
+                System.out.println("Error: Valor no válido");
+            } else {
+                num_usuari = sc.nextInt();
+            }
+        } while (!TipusCorrecte);
+
+        return num_usuari;
+    }
+
+
 }
