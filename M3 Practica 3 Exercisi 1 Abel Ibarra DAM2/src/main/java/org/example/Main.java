79ed011b7f31c5e21709753c530685a44954a113
abdel1311fatah <abdel1311fatah@gmail.com>
2023-10-16 12:41:38 +0200

    Initial commit

---
+++ M3 Practica 3 Exercisi 1 Abel Ibarra DAM2/src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        File file = new File("text.txt");
+
+//        FileReader fr = new FileReader(file);
+//        FileWriter fw = new FileWriter(file);
+
+//        String texte = String.valueOf(fr.read());
+//        String [] nom = texte.split(".");
+//        nom[1].strip();
+//        System.out.println(nom);
+
+    }
+}
